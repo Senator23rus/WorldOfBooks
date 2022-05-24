0aadec7a8f66c15f8a4fbaf93ebbeed7170d2019
senator23 <senator295@gmail.com>
2022-05-24 12:59:40 +0300

    End of Chapter 9

--- WebBooks/WebBooks/urls.py
+++ WebBooks/WebBooks/urls.py
@@ -1,11 +1,15 @@
 
 from django.contrib import admin
 from django.urls import path
-
+from django.conf.urls import url
 from catalog import views
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('admin/', admin.site.urls),
+    url(r'^books/$', views.BookListView.as_view(), name='books'),
+    url(r'^book/(?P<pk>\d+)$', views.BookDetalView.as_view(), name='book-detail'),
+    url(r'^authors/$', views.AuthorListView.as_view(), name='authors'),
+
 ]
 
